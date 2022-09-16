d9650318e5aba88c017f706afa5b8e6f22456718
Apetrolio <azimutec@gmail.com>
2022-09-16 14:26:07 -0300

    uma mudanca de inclusao no arq backendaws

--- backendS3-AWS.tf
+++ backendS3-AWS.tf
@@ -7,6 +7,12 @@ resource "aws_s3_bucket" "pastas3exa" {
 resource "aws_s3_bucket_acl" "epastas3exa" {
   bucket = aws_s3_bucket.pastas3exa.id
   acl    = "private"
+
+# colocando as tags no resource sem versionamento
+tags = {
+    Name     = "Pinhapastistoehbucket"
+    Environment = "Dev"
+  }
 }
 
 resource "aws_s3_bucket_versioning" "versioning_pastas3exa" {
