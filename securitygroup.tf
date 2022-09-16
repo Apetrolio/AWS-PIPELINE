2aaa637cad4c3ee5d4c037e4e1a60bb622c1ffcd
Apetrolio <azimutec@gmail.com>
2022-09-16 12:23:55 -0300

    coloquei na brain-aws os .tf

---
+++ securitygroup.tf
@@ -0,0 +1,41 @@
+# criando security group 
+
+resource "aws_security_group" "coloca-ssh" {
+  name        = "coloca-ssh"
+  description = "acessa as maquinas via ssh "
+
+  ingress {
+    description      = "TLS from VPC"
+    from_port        = 22
+    to_port          = 22
+    protocol         = "tcp"
+    cidr_blocks      = ["0.0.0.0/10","192.10.1.1","192.x.x.x."]
+  }
+}
+
+resource "aws_security_group" "coloca-http" {
+  name        = "coloca-http"
+  description = "acessa as maquinas via http "
+
+  ingress {
+    description      = "TLS from VPC"
+    from_port        = 80
+    to_port          = 80
+    protocol         = "tcp"
+    cidr_blocks      = ["0.0.0.0/0","192.10.1.1","192.x.x.x."]
+  }
+}
+
+
+resource "aws_security_group" "coloca-https" {
+  name        = "coloca-https"
+  description = "acessa as maquinas via https "
+
+  ingress {
+    description      = "TLS from VPC"
+    from_port        = 443
+    to_port          = 443
+    protocol         = "tcp"
+    cidr_blocks      = ["0.0.0.0/0","192.10.1.1","192.x.x.x."]
+  }
+}
